ca42ec327b245ebd2f8fc794408cdbd5746ec49a
guptarishabh2390 <guptarishabh2390@gmail.com>
2022-11-29 18:10:00 +0530

    Assignment5 | Rishabh Gupta |Added log4j2

--- src/main/java/com/zs/assignment5/MainParser.java
+++ src/main/java/com/zs/assignment5/MainParser.java
@@ -1,15 +1,20 @@
 package com.zs.assignment5;
 
-import java.io.File;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 import java.util.Date;
 
 public class MainParser {
+    private  static final Logger logger = LogManager.getLogger(MainParser.class);
     public static void main(String[] args) {
 
-        ParserController parserController=new ParserController();
-        String filePathName="log.txt";
-        Date date=new Date(2022,10,8);    //yyyy,mm,dd
-        parserController.start( filePathName , date );
+        logger.info("Hello");
+
+//        ParserController parserController=new ParserController();
+//        String filePathName="log.txt";
+//        Date date=new Date(2022,10,8);    //yyyy,mm,dd
+//        parserController.start( filePathName , date );
 
     }
 }
